f0a231616f8accbbd84d6153ea4ac73067d8d59d
kritika72005 <kritikasinghbaghel3@gmail.com>
2025-02-20 21:40:28 +0530

    minor changes

--- app.js
+++ app.js
@@ -18,8 +18,8 @@ const LocalStrategy = require("passport-local");
 const User = require("./models/user.js");
 
 const listingsRouter = require("./routes/listing.js");
-const reviewsRouter = require(".routes/review.js");
-const userRouter = require(".routes/user.js");
+const reviewsRouter = require("./routes/review.js");
+const userRouter = require("./routes/user.js");
 
 // const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
 const dbUrl = process.env.ATLASDB_URL;
